228adafb79220b16aac22c2d03790650bd1d5106
Mayara Saraiva <mayara.saraiva@outlook.com.br>
2025-03-31 21:29:14 -0300

    Creating menu interations Creating ABC class Creating Factory Level 1 Bg implementation

--- code/Const.py
+++ code/Const.py
@@ -16,3 +16,13 @@ BROWN=(92,51,23)
 RED_COLOR=(78,47,47)
 NUDE_COLOR=(255,222,173)
 SELECT_COLOR=(245,222,179)
+
+
+#Entity
+ENTITY_SPEED = {
+    'Level1Bg0': 1,
+    'Level1Bg1': 0,
+    'Level1Bg2': 0,
+    'Level1Bg3': 0,
+    'Level1Bg4': 0,
+}
